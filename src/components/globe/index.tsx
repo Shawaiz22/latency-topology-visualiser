5e491416f47d3bd719f19ec40eeb7e00fce8585f
Shawaiz Siddiqui <105994493+Shawaiz22@users.noreply.github.com>
2025-07-30 19:56:07 +0530

    Update imports

--- src/components/globe/index.tsx
+++ src/components/globe/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useTexture } from '@react-three/drei';
+import { useTexture, Html } from '@react-three/drei';
 import { useTheme } from '@/contexts/theme-context';
 import { useState } from 'react';
 
